90138a22e222b2032a2a44a170d61737cd689bc8
DevCompleteStudios <jorge.yael333@gmail.com>
2024-12-27 09:35:34 -0600

    Simulando error para probar tolerancia a fallos

--- src/main/java/com/devstudios/springcloud/msvc/products/controllers/ProductController.java
+++ src/main/java/com/devstudios/springcloud/msvc/products/controllers/ProductController.java
@@ -2,12 +2,12 @@ package com.devstudios.springcloud.msvc.products.controllers;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devstudios.springcloud.msvc.products.entities.Product;
@@ -16,7 +16,6 @@ import com.devstudios.springcloud.msvc.products.services.IProductService;
 
 
 @RestController
-@RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
@@ -29,7 +28,13 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> details( @PathVariable Long id ){
+    public ResponseEntity<Product> details( @PathVariable Long id ) throws InterruptedException {
+        if( id.equals(10L) ){
+            throw new IllegalStateException("Producto no encontrado");
+        } else if( id.equals(7L) ){
+            TimeUnit.SECONDS.sleep(3);
+        }
+
         Optional<Product> productOpt = service.findById(id);
         if( productOpt.isPresent() ) return ResponseEntity.ok(productOpt.get());
 
--- src/main/java/com/devstudios/springcloud/msvc/products/entities/Product.java
+++ src/main/java/com/devstudios/springcloud/msvc/products/entities/Product.java
@@ -25,7 +25,6 @@ public class Product {
     @Column(name="created_at")
     private LocalDate createdAt;
 
-
     public Long getId() {
         return id;
     }
